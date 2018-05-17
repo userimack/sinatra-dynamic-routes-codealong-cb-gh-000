4d885b5e0c94dc0f1926b4053dd9094764846cc6
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-17 17:14:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,7 +19,8 @@ class App < Sinatra::Base
     "Goodbye, #{@name}."
   end 
   
-  get '/multiply/:num1/:num2' do 
+  get '/multiply/:num1/:num2' do
+    @num1, @num2 = params[:num1], params[:num2]
     "#{params[:num1] * params[:num2]}"
   end
 end
